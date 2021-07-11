92d3f726616821454573782312237b3bc6f3c25e
Anub12 <73828916+Anub12@users.noreply.github.com>
2021-07-11 19:05:19 +0530

    Delete food directory

--- food/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.food-slider').slick({
-        slidesToShow:3,
-        slidesToScroll:1,
-        prevArrow: ".prev-btn",
-        nextArrow: ".next-btn"
-    })
-})
